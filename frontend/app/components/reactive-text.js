3948d5cd81d4689ba79db2f92e0c4e4689a3086e
Felix Barbalet <felixb@gmail.com>
2015-03-26 15:01:34 +1100

    Fixes #26

--- frontend/app/components/reactive-text.js
+++ frontend/app/components/reactive-text.js
@@ -2,45 +2,61 @@ import Em from 'ember';
 
 export default Em.TextField.extend({
     focusIn: function (evt) {
-        this.get('photo').set('hasFocus', true);
 
         var me = Em.$(evt.target),
             footerHeight = Em.$('.app-footer').outerHeight(),
-            ypHeight = Em.$(window).innerHeight() - footerHeight,
             ypTop = Em.$('.your-photos').position().top,
+            ypHeight = Em.$(window).innerHeight() - footerHeight - ypTop,
             photoTop = me.position().top,
-            photoBottom = photoTop + me.parent('.photo').innerHeight(),
-            y = self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
-            x = self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+            additional = 0,
+            transition = 0,
+            photoHeight = me.parent('.photo').outerHeight(),
+            y = self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-            visible_top = ypTop + y,
-            visible_bottom = visible_top + ypHeight - footerHeight,
-            offscreen = photoBottom - visible_bottom;
-
-
-        if (offscreen > 0) {
-            console.log('scrolling');
-            window.scrollTo(x,y + offscreen + footerHeight);
+        if ((photoHeight) < ypHeight) {
+            additional = (ypHeight - photoHeight) / 5;
         }
-        //console.log('visible_top',visible_top);
-        //console.log('visible_bottom',visible_bottom);
-        //console.log('photoTop',photoTop);
-        //console.log('photoBottom',photoBottom);
-        //console.log('offscreen bottom',offscreen);
+
+
+        if (photoTop + ypTop + photoHeight < y + ypHeight && photoTop > y) {
+        } else {
+            if (y < photoTop - ypTop - additional) {
+
+                $('html,body').animate(
+                    {
+                        scrollTop: photoTop - ypTop - additional
+                    },
+                    400);
+            } else {
+
+                $('html,body').animate(
+                    {
+                        scrollTop: photoTop - ypTop - additional
+                    },
+                    transition);
+            }
+
+        }
+        this.get('photo').set('hasFocus', true);
+
+        return;
     },
     focusOut: function (evt) {
         this.get('photo').set('hasFocus', false);
     },
-    keyDown: function(evt) {
+    keyDown: function (evt) {
 
         if (evt.which === 9){
 
-            var idx = Em.$(evt.target).attr('tabindex');
+            var idx = Em.$(evt.target).attr('tabindex'),
+                offset = evt.shiftKey === true? -1 : 1,
+                c = Em.$('[tabindex=' + (+idx + offset) + ']').length,
+                l = Em.$('.photo').length;
 
-            if (Em.$('[tabindex='+(+idx+1)+']').length === 0){
+            if (c === 0) {
                 evt.preventDefault();
                 evt.stopPropagation();
-                Em.$('[tabindex=1]').focus();
+                Em.$('[tabindex='+(evt.shiftKey === true? l:1)+']').focus();
             }
         }
     }
