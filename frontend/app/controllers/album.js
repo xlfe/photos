98ca3dac1294c365fe47e9e47465b40cd3912ebf
Felix Barbalet <felix@pv.tl>
2015-04-07 07:16:12 +1000

    Apply visibility above viewport as well

--- frontend/app/controllers/album.js
+++ frontend/app/controllers/album.js
@@ -104,13 +104,11 @@ function isElementInViewport (el) {
         el = el[0];
     }
 
-    var rect = el.getBoundingClientRect();
+    var rect = el.getBoundingClientRect(),
+        height = (window.innerHeight || document.documentElement.clientHeight)*4;
 
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)*4 && /*or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        rect.top >= (-height/1.5) && rect.bottom <= height
     );
 }
 
